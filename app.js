c4c6465bf703ab091f7f819283cd7e6b3a67d3ea
samade747 <samad.e747@gmail.com>
2023-11-04 02:20:32 +0500

    15115

---
+++ app.js
@@ -0,0 +1,14 @@
+var getul = document.getElementById('ul');
+
+function additem(){
+    var ca = document.getElementById('textinput');
+    var li = document.createElement('li');
+    var litext = document.createTextNode(ca.value);
+    li.appendChild(litext)
+    getul.appendChild(li)
+    ca.value = ''
+    
+
+
+
+}
