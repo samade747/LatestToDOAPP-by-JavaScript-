adf4669e2418697e70491d9f3a221ccbe0841f60
samade747 <samad.e747@gmail.com>
2023-11-04 04:05:00 +0500

    29+46564

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var getul = document.getElementById('ul');
+var clearallitems = document.getElementById('clearitems')
 
 function additem(){
     var ca = document.getElementById('textinput');
@@ -7,8 +8,33 @@ function additem(){
     li.appendChild(litext)
     getul.appendChild(li)
     ca.value = ''
+    var deletebtn = document.createElement('button');
+    var deletebtntext = document.createTextNode('del');
+    deletebtn.appendChild(deletebtntext);
+    li.appendChild(deletebtn);
+    deletebtn.setAttribute('onclick', 'del(this.parentNode)');
+    var editbtn = document.createElement('button');
+    var editbtntext = document.createTextNode('Edit')
+    editbtn.appendChild(editbtntext);
+    editbtn.setAttribute('onclick', 'editfun(this)')
+    li.appendChild(editbtn)
+    deletebtn.setAttribute( 'class', 'btn btn-danger aaa')
+    deletebtn.setAttribute('onclick', 'del(this)');
+    editbtn.setAttribute('class', 'btn btn-info aaa')
+
+    var clibutton = clearallitems.createElement('Button')
+    var clearallitemstext = createTextNode('clear items')
+    clibutton.appendChild(clearallitemstext);
+    clearallitems.setAttribute('onclick', 'deleteall()')
+
+}
+
+
+
+
+
+
+function deleteall(){
     
-
-
-
+    getul.innerHTML = ''
 }
